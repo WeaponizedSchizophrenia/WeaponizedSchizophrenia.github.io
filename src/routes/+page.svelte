1246a006458377f16fd100f5aef7a2889edcb5be
WeaponizedSchizophrenia <laurynasramankevicius2@gmail.com>
2024-10-01 12:20:07 +0300

    Fix routing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+	import { base } from '$app/paths';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<a href="/test-route">TEST ROUTE</a>
+<a href="{base}/test-route">TEST ROUTE</a>
